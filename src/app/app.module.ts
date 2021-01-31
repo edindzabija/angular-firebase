f959791d3a1c0cb1f270c0e59f5ab4fcf10e566e
Edin Dzabija <edindzabija@gmail.com>
2021-01-31 22:00:14 +0100

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms/';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +21,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
-
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,6 +44,8 @@ import { TrainingService } from './training/training.service';
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [AuthService, TrainingService],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBeiXNVI1C-L6uLz-amRao5BEqM496WASI",
+    authDomain: "ng-fitness-tracker-3c060.firebaseapp.com",
+    projectId: "ng-fitness-tracker-3c060",
+    storageBucket: "ng-fitness-tracker-3c060.appspot.com",
+    messagingSenderId: "1038683433085",
+    appId: "1:1038683433085:web:f4eb58f817b6ef255ce914",
+    measurementId: "G-CVVB3TRN58"
+  }
 };
 
 /*
