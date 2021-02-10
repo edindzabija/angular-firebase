97fb23205552d03448fd608f328e6a921e59cd6b
Edin Dzabija <edindzabija@gmail.com>
2021-02-10 22:37:10 +0100

    auth service refactor snackbar via ui service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,7 +17,6 @@ export class AuthService {
     private router: Router,
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
-    private snackbar: MatSnackBar,
     private uiService: UIService
   ) {}
 
@@ -45,9 +44,7 @@ export class AuthService {
       })
       .catch((error) => {
         this.uiService.loadingStateChanged.next(false);
-        this.snackbar.open(error.message, null, {
-          duration: 3000,
-        });
+        this.uiService.showSnackbar(error.message, null, 3000);
       });
   }
 
@@ -60,9 +57,7 @@ export class AuthService {
       })
       .catch((error) => {
         this.uiService.loadingStateChanged.next(false);
-        this.snackbar.open(error.message, null, {
-          duration: 3000,
-        });
+        this.uiService.showSnackbar(error.message, null, 3000);
       });
   }
 
