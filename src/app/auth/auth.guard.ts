48a321c0e6fc3833285e23a14aa268aeda72c7a2
Edin Dzabija <edindzabija@gmail.com>
2021-02-11 01:19:43 +0100

    moved auth guard Auth guard in app routing module, lazy loading the route optimized

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,16 +2,27 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
+  //can activate not required - legacy
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  canLoad(route: Route) {
     if (this.authService.isAuth()) {
       return true;
     } else {
